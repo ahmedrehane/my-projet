fdb8a62aa862124f80e94599617624a2a5692ec2
ahmedrehane <rehaneahmed49@gmail.com>
2024-01-14 00:04:07 +0100

    price refactor

--- src/Sidebar/Categorie/Categorie.jsx
+++ src/Sidebar/Categorie/Categorie.jsx
@@ -1,17 +1,55 @@
 import Input from '../../Components/Input'
 import './Categorie.css'
 
-const Categorie = () =>{
+const Categorie = ({handlChange}) =>{
     return(
         <>
         <div>
             <h2 className='sidebar-title'>Categorie</h2>
 
             <div>
-           <Input/>
-           <Input/>
-           <Input/>
-           <Input/>
+           <label className='sidebar-label-container'>
+            <input onChange={handlChange} type='radio' value="" name='test' />
+            <span className='checkmark'></span>All
+
+            
+           </label>
+
+
+
+           <Input
+           
+            handlechange={handlChange}
+            value='sneakers'
+            title='Sneakers'
+            name='test'
+
+           />
+           <Input
+           
+           handlechange={handlChange}
+           value='falts'
+           title='Falts'
+           name='test'
+
+          />
+          <Input
+           
+           handlechange={handlChange}
+           value='sandals'
+           title='Sandals'
+           name='test'
+
+          />
+          <Input
+           
+           handlechange={handlChange}
+           value='heels'
+           title='Heels'
+           name='test'
+
+          />
+          
            
             </div>
         </div>
